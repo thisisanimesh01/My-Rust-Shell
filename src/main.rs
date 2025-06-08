5a3bfcfe17a998a6cba787bd708a37b91100b9c2
Animesh Yadav <thisisanimesh01@gmail.com>
2025-06-09 05:25:55 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,29 @@ use std::path::Path;
 
 fn main() {
     loop {
-        // Show prompt
+        // prompt
         print!("rustsh> ");
         io::stdout().flush().unwrap();
 
-        // Get user input
+        //user input
         let mut input = String::new();
         if io::stdin().read_line(&mut input).is_err() {
             eprintln!("Failed to read input");
             continue;
         }
 
-        // Trim and skip if empty
+        // Trim & skip if empty
         let input = input.trim();
         if input.is_empty() {
             continue;
         }
 
-        // Split input into command and arguments
+        // command and arguments
         let mut parts = input.split_whitespace();
         let command = parts.next().unwrap();
         let args: Vec<&str> = parts.collect();
 
-        // Handle built-in commands
+        // for built-in commands
         match command {
             "cd" => {
                 let new_dir = args.get(0).unwrap_or(&"/");
@@ -40,7 +40,7 @@ fn main() {
                 break;
             }
             _ => {
-                // Run external command
+                // to run external command
                 match Command::new(command)
                     .args(&args)
                     .stdin(Stdio::inherit())
